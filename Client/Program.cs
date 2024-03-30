8a1dd206fd4fbc44f353c950633468d656032e72
user <user@WIN-AKH758KNU81>
2024-03-30 19:13:18 +1000

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using Network;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            SentMessage(args[0], args[1]);
+        }
+
+
+        public static void SentMessage(string From, string ip)
+        {
+            RequestSocket client = new RequestSocket(">tcp://" + ip + ":12345");
+
+            bool isRecived = false;
+            while (true)
+            {
+                string messageText;
+                do
+                {
+                    Console.Clear();
+                    if (isRecived)
+                    {
+                        Console.WriteLine("Cообщение доставлено!");
+                        isRecived = false;
+                    }
+                    Console.WriteLine("Введите сообщение.");
+                    messageText = Console.ReadLine();
+
+                }
+                while (string.IsNullOrEmpty(messageText));
+                if (messageText == "Exit")
+                    break;
+                Message message = new Message() { Text = messageText, NicknameFrom = From, NicknameTo = "Server", DateTime = DateTime.Now };
+                string json = message.SerializeMessageToJson();
+
+                client.SendFrame(json);
+                if(client.ReceiveFrameString() == "ok")
+                    isRecived = true;
+            }
+
+            client.Close();
+        }
+    }
+}
---
+++ Network/Program.cs
@@ -0,0 +1,47 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Network
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server("Hello");
+        }
+
+        public static void Server(string name)
+        {
+            ResponseSocket server = new ResponseSocket("@tcp://*:12345");
+            
+            Console.WriteLine("Сервер ждет сообщение от клиента");
+            var t = new Thread(WaitAnyKey);
+            t.Start();
+
+
+            while (true) 
+            {
+                string messageText = server.ReceiveFrameString();
+                
+                if (messageText != null)
+                {
+                    Message message = Message.DeserializeFromJson(messageText);
+                    message.Print();
+                    server.SendFrame("ok");
+                }
+            }
+        }
+        private static void WaitAnyKey()
+        {
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+                System.Environment.Exit(0);
+        }
+    }
+}
